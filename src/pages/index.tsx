14aabce37c5ada60adbd7ffa58cf590afc4d1776
João Gilberto <gjoao11.dev@gmail.com>
2021-11-09 10:06:26 -0300

    Create users page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import {
   Flex,
   Button,
   Stack,
-  LightMode,
   FormLabel,
   FormControl
 } from '@chakra-ui/react'
@@ -10,37 +9,35 @@ import { Input } from '../components/Form/Input'
 
 export default function Home() {
   return (
-    <LightMode>
+    <Flex
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
       <Flex
-        w="100vw"
-        h="100vh"
-        align="center"
-        justify="center"
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius="8"
+        flexDir="column"
       >
-        <Flex
-          as="form"
-          width="100%"
-          maxWidth={360}
-          bg="gray.800"
-          p="8"
-          borderRadius="8"
-          flexDir="column"
-        >
-          <Stack spacing="4">
-            <Input name="email" type="email" label="E-mail" />
-            <Input name="password" type="password" label="Senha" />
-          </Stack>
+        <Stack spacing="4">
+          <Input name="email" type="email" label="E-mail" />
+          <Input name="password" type="password" label="Senha" />
+        </Stack>
 
-          <Button
-            type="submit"
-            mt="6"
-            colorScheme="pink"
-            size="lg"
-          >
-            Entrar
-          </Button>
-        </Flex>
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+        >
+          Entrar
+        </Button>
       </Flex>
-    </LightMode>
+    </Flex>
   )
 }
